51dc925bec8bdb5f75fa2de8706ce7bcf20a4339
shahbaz <mohdshahbaz705454@gmail.com>
2023-03-07 15:40:18 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("The sum of these number is : ");
+        int num1=2;
+        int num2=5;
+        int num3=7;
+        int num4=10;
+
+        int sum=(num1+num2+num3+num4);
+        System.out.println(sum);
+
+
+
+    }
+
+
+}
