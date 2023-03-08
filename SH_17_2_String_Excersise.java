15e7166e0a783f42cfb4879748811b4443fa6186
shahbaz <mohdshahbaz705454@gmail.com>
2023-03-08 16:43:09 +0530

    new chapter added

---
+++ SH_17_2_String_Excersise.java
@@ -0,0 +1,49 @@
+package com.company;
+
+
+
+public class SH_17_2_String_Excersise {
+    public static void main(String[]args){
+// PROBLEM 1//
+//        String name= "Shahbaz";
+//        name = name.toLowerCase();
+//        System.out.println(name);
+
+
+
+
+//      PROBLEM 2//
+
+//   String text ="This is spaced sentence ";
+//
+//   text = text.replace(" ", "_");
+//        System.out.println(text);
+
+
+//PROBLEM 3//
+
+//        String name = "My name is <|name|>";
+//        name = name.replace("<|name|>", "shahbaz");
+//        name= name.toUpperCase();
+//
+//        System.out.println(name);
+
+
+
+//        PROBLEM 4
+
+//        String text ="This String Contains Double And  Triple Spaces";
+//        System.out.println(text.indexOf("  "));
+//        System.out.println(text.indexOf("   "));
+
+
+// PROBLEM 4
+
+  String letter = "Dear shahbaz,\n\tThanks for choosing java programming language.\n\tThanks";
+        System.out.println(letter.toUpperCase());
+
+
+    }
+
+
+}
---
+++ SH_18_Excersice_loops.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class SH_18_Excersice_loops {
+    public static void main(String []args){
+
+
+//------------ Programme 1 // -------make a program that play rock, sessior, and paper game with user ---------
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
