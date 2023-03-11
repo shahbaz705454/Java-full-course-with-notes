2b5da39527607f29bc81fccf731da088bd23403b
shahbaz <mohdshahbaz705454@gmail.com>
2023-03-11 18:09:42 +0530

    program in method

--- SH_31_Methods.java
+++ SH_31_Methods.java
@@ -3,6 +3,14 @@ package com.company;
 
 
 public class SH_31_Methods {
+
+                //    WHAT IS METHOD //
+//A method is a block of code which only runs when it is called.
+// You can pass data, known as parameters, into a method.
+// Methods are used to perform certain actions,
+// and they are also known as functions.
+
+
     static void sorted(int[] number){
 
              boolean sorted = true;
