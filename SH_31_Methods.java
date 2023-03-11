d09ad1b07cea7df624fd798616f5cbff4320bc69
shahbaz <mohdshahbaz705454@gmail.com>
2023-03-11 18:07:13 +0530

    program in method

--- SH_31_Methods.java
+++ SH_31_Methods.java
@@ -1,4 +1,40 @@
 package com.company;
 
+
+
 public class SH_31_Methods {
+    static void sorted(int[] number){
+
+             boolean sorted = true;
+            for (int i =0; i<number.length-1; i++){
+                if (number[i]>number[i+1]){
+                    sorted = false;
+                    break;
+                }
+            }
+
+            if (sorted){
+                System.out.println("Array is sorted");
+
+
+            }else {
+                System.out.println("Array is not sorted");
+
+            }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+
+        int[] mat = {1,2,3,4,5,6,7,8,9};
+        sorted(mat);
+
+
+    }
+
+
+
 }
